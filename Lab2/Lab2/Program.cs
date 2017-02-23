ebc7c2ab6a5de5429b0cf9d2fcb43ab23216a864
shoujizhang977 <shoujizhang977@gmail.com>
2017-02-23 16:46:20 +0800

    Lab2

---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,52 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader sr = new StreamReader(@"C:\Users\admin\Downloads\lab-2-zi-chuan-chu-li-shoujizhang977\Data\Input.txt"))
+            {
+                int ss = 0;
+                while (sr.Peek() != -1)
+                {
+                    string line = sr.ReadLine();
+                    foreach (var ch in line)
+                    {
+                        if (ch == '?')
+                        {
+                            Console.Write('?' + "\n");
+                            ss++;
+                        }
+                        else if (ch == '.')
+                        {
+                            Console.Write('.' + "\n");
+                            ss++;
+                        }
+                        else if (ch == '!')
+                        {
+                            Console.Write('!' + "\n");
+                            ss++;
+                        }
+                        else if (ss != 0)
+                        {
+                            Console.Write("");
+                            ss = 0;
+                        }
+                        else
+                            Console.Write(ch);
+                    }
+
+                }
+            }
+            Console.Read();
+        }
+    }
+}
